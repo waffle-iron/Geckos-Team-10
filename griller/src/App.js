39e8554919e23ca5d3ec53db726d353b972ab22c
Igor Katselenbogen <underwoodkats@gmail.com>
2018-02-28 20:17:36 +0300

    navbar

---
+++ griller/src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/lib/Navbar';
+import NavItem from 'react-bootstrap/lib/NavItem';
+import NavDropdown from 'react-bootstrap/lib/NavDropdown';
+import MenuItem from 'react-bootstrap/lib/MenuItem';
+import Nav from 'react-bootstrap/lib/Nav';
+
+
+
+
+class App extends React.Component {
+
+    render() {
+    return (
+        <Navbar>
+            <Navbar.Header>
+                <Navbar.Brand>
+                    <a href="#home">React-Bootstrap</a>
+                </Navbar.Brand>
+            </Navbar.Header>
+            <Nav>
+                <NavItem eventKey={1} href="#">
+                    Link
+                </NavItem>
+                <NavItem eventKey={2} href="#">
+                    Link
+                </NavItem>
+                <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+                    <MenuItem eventKey={3.1}>Action</MenuItem>
+                    <MenuItem eventKey={3.2}>Another action</MenuItem>
+                    <MenuItem eventKey={3.3}>Something else here</MenuItem>
+                    <MenuItem divider />
+                    <MenuItem eventKey={3.4}>Separated link</MenuItem>
+                </NavDropdown>
+            </Nav>
+        </Navbar>
+    );
+  }
+}
+
+export default App;
